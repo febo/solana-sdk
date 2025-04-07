f2ecaccbc620cf64e99d9a4faed3b43c47d72e8e
febo <febo@anza.xyz>
2025-04-07 16:18:54 -0700

    Add declare_id macro

--- address/src/lib.rs
+++ address/src/lib.rs
@@ -28,3 +28,44 @@ pub const MAX_SEEDS: usize = 16;
 /// [ed25519]: https://ed25519.cr.yp.to/
 /// [pdas]: https://solana.com/docs/core/cpi#program-derived-addresses
 pub type Address = [u8; ADDRESS_BYTES];
+
+/// Convenience macro to declare a static address and functions to interact with it.
+///
+/// This macro is useful to declare a constant representing a program ID for a
+/// Solana program.
+///
+/// # Example
+///
+/// ```
+/// # // wrapper is used so that the macro invocation occurs in the item position
+/// # // rather than in the statement position which isn't allowed.
+/// use solana_address::{declare_id, Address};
+///
+/// # mod program {
+/// #   use solana_address::declare_id;
+/// declare_id!([0; 32]);
+/// # }
+/// # use program::id;
+///
+/// let address = [0; 32];
+/// assert_eq!(id(), address);
+/// ```
+#[macro_export]
+macro_rules! declare_id {
+    ( $id:expr ) => {
+        #[doc = "The constant program ID."]
+        pub const ID: $crate::Address = $id;
+
+        #[doc = "Returns `true` if the given address is equal to the program ID."]
+        #[inline]
+        pub fn check_id(id: &$crate::Address) -> bool {
+            id == &ID
+        }
+
+        #[doc = "Returns the program ID."]
+        #[inline]
+        pub const fn id() -> $crate::Address {
+            ID
+        }
+    };
+}
