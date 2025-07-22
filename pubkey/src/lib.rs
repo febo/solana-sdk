63da4e710e39e6304d5e90467908692fa72f0f37
febo <febo@anza.xyz>
2025-07-22 10:27:22 +0100

    Use address crate

---
+++ pubkey/src/lib.rs
@@ -0,0 +1,23 @@
+//! Solana account addresses.
+#![no_std]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
+#![allow(clippy::arithmetic_side_effects)]
+
+// If target_os = "solana", then this panics so there are no dependencies.
+// When target_os != "solana", this should be opt-in so users
+// don't need the curve25519 dependency.
+#[cfg(any(target_os = "solana", feature = "curve25519"))]
+pub use solana_address::bytes_are_curve_point;
+/// New random Pubkey for tests and benchmarks.
+#[cfg(all(feature = "rand", not(target_os = "solana")))]
+pub use solana_address::new_rand;
+#[cfg(target_os = "solana")]
+pub use solana_address::syscalls::*;
+pub use solana_address::{
+    declare_deprecated_id, declare_id,
+    error::{ParsePubkeyError, PubkeyError},
+    pubkey, Pubkey, MAX_SEEDS, MAX_SEED_LEN, PUBKEY_BYTES,
+};
+#[cfg(all(feature = "rand", not(target_os = "solana")))]
+pub use solana_address::{PubkeyHasher, PubkeyHasherBuilder};
