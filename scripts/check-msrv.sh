885b1bf4115c74b89636596eed37cf6a36828be5
febo <febo@anza.xyz>
2025-11-20 21:04:48 +0000

    Update msrv check

--- scripts/check-msrv.sh
+++ scripts/check-msrv.sh
@@ -5,4 +5,21 @@ here="$(dirname "$0")"
 src_root="$(readlink -f "${here}/..")"
 cd "${src_root}"
 
-./cargo minimum check
+source "./scripts/read-cargo-variable.sh"
+
+find "$src_root" -type d -print0 | while read -r -d '' dir; do
+  crate="${dir##*/}"
+  # Skip directories that do not contain a Cargo.toml
+  if [[ -e "$crate/Cargo.toml" ]]; then
+    # Read the MSRV from the crate
+    minimum_version=$(readCargoVariable rust-version "$crate/Cargo.toml" 2>/dev/null)
+
+    # If the crate does not specify a rust-version, fall back to
+    # the "program" crate MSRV
+    if [[ -z "$minimum_version" ]]; then
+      minimum_version=$(readCargoVariable rust-version "program/Cargo.toml")
+    fi
+
+    cargo +$minimum_version check
+  fi
+done
