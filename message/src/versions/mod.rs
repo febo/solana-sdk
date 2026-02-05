649830eb46ff20e8ef58b8dcd0b6b9fa0bcde195
febo <febo@anza.xyz>
2026-02-05 18:25:20 +0000

    Update abi value

--- message/src/versions/mod.rs
+++ message/src/versions/mod.rs
@@ -52,7 +52,7 @@ pub const MESSAGE_VERSION_PREFIX: u8 = 0x80;
 /// format.
 #[cfg_attr(
     feature = "frozen-abi",
-    frozen_abi(digest = "3FVVnsLRS9Ue7MwhQPLxod48B1HxvDuX4nt4UhKV4jcs"),
+    frozen_abi(digest = "6CoVPUxkUvDrAvAkfyVXwVDHCSf77aufm7DEZy5mBVeX"),
     derive(AbiEnumVisitor, AbiExample)
 )]
 #[derive(Debug, PartialEq, Eq, Clone)]
