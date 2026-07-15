ce4d62f8fff8b91708d0dbc8005325b7f0ffa0c9
febo <febo@anza.xyz>
2026-07-15 11:38:53 +0100

    Export GetSysvar trait

--- rent/src/sysvar.rs
+++ rent/src/sysvar.rs
@@ -1,8 +1,7 @@
-pub use solana_sdk_ids::sysvar::rent::{check_id, id, ID};
-use {
-    crate::Rent,
-    solana_get_sysvar::{impl_get_sysvar, GetSysvar},
-    solana_sysvar_id::impl_sysvar_id,
+use {crate::Rent, solana_get_sysvar::impl_get_sysvar, solana_sysvar_id::impl_sysvar_id};
+pub use {
+    solana_get_sysvar::GetSysvar,
+    solana_sdk_ids::sysvar::rent::{check_id, id, ID},
 };
 
 impl_sysvar_id!(Rent);
